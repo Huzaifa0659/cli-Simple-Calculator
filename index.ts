ca3075b20eb3573ada2999da887a382a1c87d12a
Huzaifa Ramzan <huzaifahuz061@gmail.com>
2024-04-01 02:19:22 +0500

    Added cli-Simple-Calculator

---
+++ index.ts
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter Your First Number", type: "number", name: "firstNumber" },
+  { message: "Enter Your Second Number", type: "number", name: "secondNumber" },
+  {
+    message: "Select One Of The Following Operations",
+    type: "list",
+    name: "operation",
+    choices: ["Addition", "Multiplication", "Subtraction", "Division"],
+  },
+]);
+
+//conditional statements
+
+if (answer.operation === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operation === "Subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operation === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operation === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else {console.log("Plz Select A Valid Operation");}
